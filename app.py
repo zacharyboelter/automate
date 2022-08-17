36acd2b89a3c5df33bfb4794b85f67d4b2c95a36
Zachary Boelter <zboelter@gmail.com>
2022-08-17 11:02:41 -0700

    continue

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 # while loop
 
-name = ''
-while True:
-    print('Please type your name.')
-    name = input()
-    if name == 'your name':
-        break
-print('Thank you')    
+# name = ''
+# while True:
+#     print('Please type your name.')
+#     name = input()
+#     if name == 'your name':
+#         break                               #break statement so the infinite loop will close out
+# print('Thank you')    
+
+spam = 0
+while spam < 5:
+    spam = spam + 1
+    if spam == 3:
+        continue
+    print('Spam is ' + str(spam))
