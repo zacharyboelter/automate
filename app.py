59d3a0d91a45ac3de684958f639a3ff07e2af056
Zachary Boelter <zboelter@gmail.com>
2022-08-17 11:29:00 -0700

    for loop

--- app.py
+++ app.py
@@ -8,9 +8,16 @@
 #         break                               #break statement so the infinite loop will close out
 # print('Thank you')    
 
-spam = 0
-while spam < 5:
-    spam = spam + 1
-    if spam == 3:
-        continue
-    print('Spam is ' + str(spam))
+# spam = 0
+# while spam < 5:
+#     spam = spam + 1
+#     if spam == 3:
+#         continue    #continue statement will skip over 3 while printing the rest
+#     print('Spam is ' + str(spam))
+
+# for loop
+
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
