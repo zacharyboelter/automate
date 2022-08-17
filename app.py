0ba361a3e3cee35e48d532b603dc76a1fd268af0
Zachary Boelter <zboelter@gmail.com>
2022-08-17 13:09:17 -0700

    magic 8 ball game

--- app.py
+++ app.py
@@ -30,6 +30,40 @@
 # print(random_num)
 
 
-import pyperclip
-pyperclip.copy('The text to be copied to the clipboard.')
-pyperclip.paste()
+#functions
+
+# def hello():
+#     print('howdy')
+#     print('hello world')
+
+# hello()
+# hello()
+# hello()
+# hello()
+
+import random
+
+def getAnswer(answerNumber):
+    if answerNumber == 1:
+        return 'It certainly is'
+    elif answerNumber == 2:
+        return 'It is decidedly so.'
+    elif answerNumber == 3:
+        return 'Yes'
+    elif answerNumber == 4:
+        return 'Reply hazy, try again'
+    elif answerNumber == 5:
+        return 'Ask again later'
+    elif answerNumber == 6:
+        return 'Concentrate and ask again'
+    elif answerNumber == 7:
+        return 'My answer is no'
+    elif answerNumber == 8:
+        return 'Outlook not so good'
+    elif answerNumber == 9:
+        return 'Very doubtful'
+
+print('Think of a question to ask me.')
+input()
+
+print(getAnswer(random.randint(1, 9)))
