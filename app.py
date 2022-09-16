a7c9a7a7b54248e72eb50fcbbfaa38848481096a
Zachary Boelter <zboelter@gmail.com>
2022-09-16 13:44:58 -0700

    next day

--- app.py
+++ app.py
@@ -264,11 +264,17 @@ import pprint
 #         break
 #     print('Passwords can have only numbers and letters you twat!')
 
-def printPicnic(itemsDict, leftWidth, rightWidth):
-    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))               #prints title for table in center with dashes on left and right
-    for k, v in itemsDict.items():                                          #for loop that iterates over key value pairs
-        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))           #print the key with ... to fill space, then adjust values to right side
+# def printPicnic(itemsDict, leftWidth, rightWidth):
+#     print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))               #prints title for table in center with dashes on left and right
+#     for k, v in itemsDict.items():                                          #for loop that iterates over key value pairs
+#         print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))           #print the key with ... to fill space, then adjust values to right side
 
-picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
-printPicnic(picnicItems, 12, 5)
-printPicnic(picnicItems, 12, 20)
+# picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000, 'plates': 42, 'bottles of wine': 69, 'glasses': 4}
+# printPicnic(picnicItems, 12, 5)
+# printPicnic(picnicItems, 20, 15)
+
+
+spam = 'SpamSpamBaconSpamEggsSpamSpam'
+print(spam.strip('ampS'))               #the order of elements you want striped from the ends doesnt matter, will remove in any order
+print(spam.strip('pmSa'))
+print(spam.strip('Smap'))
